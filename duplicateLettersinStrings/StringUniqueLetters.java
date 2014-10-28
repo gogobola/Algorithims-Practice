c338a564aea3c7fbdede83dec86ee91666dd5bcf
Bola <abaki@udel.edu>
2014-10-27 20:20:03 -0400

    First Correct solution at finding duplicate letters in a string

---
+++ duplicateLettersinStrings/StringUniqueLetters.java
@@ -0,0 +1,63 @@
+package duplicateLettersinStrings;
+import java.util.Arrays;
+
+public class StringUniqueLetters {
+String str;
+char[] charArray;
+// n + n log n + n
+public StringUniqueLetters(String str){
+	this.str = str;
+}
+
+public boolean isUnique() {
+	//Runtime for conversion to charArray is n
+	charArray = this.str.toCharArray();
+	//Runtime for built in sort is n log n
+	Arrays.sort(charArray);
+	
+	boolean result = true;
+
+	for(int i = 0; i < charArray.length - 1;i++) {
+		//Once Sorted any duplicates will be next to each other so check if so, not unique
+		if(charArray[i] == charArray[i+1]){
+			
+		result = false;
+		
+				}
+			
+	}
+	
+	return result;
+	
+}
+
+public static void main(String[] args){
+	
+	StringUniqueLetters test = new StringUniqueLetters("azqwxcerfvbgtyhnjmkiust");
+	StringUniqueLetters test2 = new StringUniqueLetters("tesnm");
+
+	if(test.isUnique()){
+		
+		System.out.println("This String is Unique");
+		
+	}
+	else {
+		
+		System.out.println("This String is not Unique");
+		
+	}
+
+	if(test2.isUnique()){
+		
+		System.out.println("This String is Unique");
+		
+	}
+	else {
+		
+		System.out.println("This String is not Unique");
+		
+	}
+
+}
+	
+}
